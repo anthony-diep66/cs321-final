9c1d4049430d9894f5c9921df4ada0f51f5e808e
BoiseState Student <student@u.boisestate.edu>
2022-04-27 18:45:10 -0600

    Closes #9

--- src/main/java/cs321/btree/BTree.java
+++ src/main/java/cs321/btree/BTree.java
@@ -1,9 +1,15 @@
 package cs321.btree;
 
-public class BTree<E>
-{
-	private int height;
-	public BTree() {
-		
-	}
+public class BTree<E>{
+
+    private int orderOfTree;
+    private int lengthOfSubstring;      // 1<= k <= 31
+    private int root;                   // pointer should be of type "int"
+
+    BTree(int degree, int substringLength){
+            this.orderOfTree = order;
+            this.lengthOfSubstring = substringLength;
+            this.root = null;
+    }
+
 }
