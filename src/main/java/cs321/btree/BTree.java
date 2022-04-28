1928285a7e3f9e820f7e79e87519ecfab03f0030
BoiseState Student <student@u.boisestate.edu>
2022-04-28 01:14:32 -0600

    Closes #16

--- src/main/java/cs321/btree/BTree.java
+++ src/main/java/cs321/btree/BTree.java
@@ -2,14 +2,65 @@ package cs321.btree;
 
 public class BTree{
 
-    private int degreeOfTree;
+    private int degree;
     private int lengthOfSubstring;      // 1<= k <= 31
     private BTreeNode root;                   // pointer should be of type "int"
 
     BTree(int degree, int substringLength){
-            this.degreeOfTree = degree;
+            this.degree = degree;
             this.lengthOfSubstring = substringLength;
             this.root = null;
+    }
+
+    /**
+     * (NEEDS TO BE TESTED) - I suspect there might be some indexing issues, especially 
+     * when parentNode child pointers are being set.
+     * split - Given a non-full parent BTreeNode, a full child node, and the index
+     *      of the child node in parentNode's childPointers, this method will split the
+     *      fullChildNode into two separate BTreeNodes and redirect the parentNode 
+     *      child pointers to correct locations
+     * @param parentNode
+     * @param fullChildNode
+     * @param idxOfChildNode
+     */
+    void split(BTreeNode parentNode, BTreeNode fullChildNode, int idxOfChildNode){
+        BTreeNode newNode = new BTreeNode(this.degree);
+        BTreeNode y = fullChildNode;
+        if( y.getLeafStatus() == true ){
+            newNode.setLeafStatus(true);
+        }
+        else{
+            newNode.setLeafStatus(false);
+        }
+        newNode.setNumKeys(degree - 1);
+        
+        for(int i = 0; i < degree - 1; i++){
+            newNode.setKeyAt(i, y.getKeyAt(i + degree));
+        }
+
+        if( y.getLeafStatus() == false ){
+            for(int i = 0; i < degree; i++){
+                
+                newNode.setChildPointerAt(i, y.getChildPointerAt(i + degree));
+            }
+        }
+
+        y.setNumKeys(degree - 1);
+
+        for(int j = parentNode.getNumKeys(); j >= idxOfChildNode + 1; j--){
+            parentNode.setChildPointerAt(j + 1, parentNode.getChildPointerAt(j));
+        }
+
+
+        parentNode.setChildPointerAt(idxOfChildNode + 1, newNode);
+        for(int j = parentNode.getNumKeys() - 1; j >= idxOfChildNode; j--){
+            parentNode.setKeyAt(j + 1, parentNode.getKeyAt(j));
+
+        }
+
+        parentNode.setKeyAt(idxOfChildNode, fullChildNode.getKeyAt(this.degree - 1));
+        parentNode.incrementNumKeys();
+
     }
 
 }
--- src/main/java/cs321/btree/BTreeNode.java
+++ src/main/java/cs321/btree/BTreeNode.java
@@ -5,18 +5,103 @@ public class BTreeNode {
   
     private TreeObject[] keys;
     private BTreeNode[] childPointers;
-    private int currentNumberOfKeysStored;
+    private int numKeys;
     private boolean isLeaf;
     private int degree;
 
     BTreeNode(int degree){
         this.keys = new TreeObject[degree - 1];
         this.childPointers = new BTreeNode[degree];
-        this.currentNumberOfKeysStored = 0;
+        this.numKeys = 0;
         this.isLeaf = true;
         this.degree = degree;
 
     }
+
+
+
+    /**
+     * getChildPointerAt - Takes a given index and returns the BTreeNode
+     *      stored in that position of childPointers
+     * @param idx
+     * @return
+     */
+    BTreeNode getChildPointerAt(int idx){
+        return this.childPointers[idx];
+    }
+
+    /**
+     * setChildPointerAt - Takes a given index and a BTreeNode, sets the index
+     *       of this BTreeNode.childPointers to that passed in value
+     * @param idx
+     * @param value
+     */
+    void setChildPointerAt(int idx, BTreeNode value){
+        this.childPointers[idx] = value;
+    }
+
+    /**
+     * setKeyAt - Takes a given index and a TreeObject to be set. Sets the 
+     *      passed TreeObject into this BTreeNode.keys at position idx
+     * @param idx
+     * @param value
+     */
+    void setKeyAt(int idx, TreeObject value){
+        this.keys[idx] = value;
+    }
+
+    /**
+     * getKeyAt - Takes a given index and returns the key stored in
+     *      this BTreeNode.keys.
+     * @param idx
+     * @return
+     */
+    TreeObject getKeyAt(int idx){
+        return this.keys[idx];
+    }
+
+    /**
+     * setNumKeys - Takes a value and set the numKeys field to that value
+     * @param value
+     */
+    void setNumKeys(int value){
+        this.numKeys = value;
+    }
+
+    /**
+     * getNumKeys - returns the current number of keys that this node has
+     * @return
+     */
+    int getNumKeys(){
+        return this.numKeys;
+    }
+
+    /**
+     * incrementNumKeys - increments the number of keys of this node
+     */
+    void incrementNumKeys(){
+        this.numKeys++;
+    }
+
+    /**
+     * setLeafStatus - sets the isKey field of this node
+     *
+     * @param newStatus
+     */
+    void setLeafStatus(boolean newStatus){
+        this.isLeaf = newStatus;
+    }
+	
+    /**
+     * getLeafstatus - returns the current value of this node's isLeaf field
+     *
+     * @return
+     */
+    boolean getLeafStatus(){
+        return this.isLeaf;
+    }
+
+
 
   
 }
