da91066818845506352f021c649f4a3926b4abe4
BoiseState Student <student@u.boisestate.edu>
2022-04-30 00:43:20 -0600

    Closes #24

--- src/main/java/cs321/create/GeneBankCreateBTree.java
+++ src/main/java/cs321/create/GeneBankCreateBTree.java
@@ -42,4 +42,46 @@ public class GeneBankCreateBTree
         return null;
     }
 
+/**
+     * parseFile - given a gbk file and the desired length of each substring (k)
+     *      parse through the file and insert each substring into the BTree field
+     * 
+     * @param fileName
+     * @param lengthOfSubstring
+     * @throws IOException
+     */
+    static void parseFile(String fileName, int lengthOfSubstring) throws IOException{
+        BufferedReader br = new BufferedReader(new FileReader("test1.gbk"));
+        String line = "";
+        int k = 6; 
+        do{
+            if( line.contains("ORIGIN") ){
+                while( line.equals("//") == false ){
+                    line = br.readLine();  
+                    tokenizer = new StringTokenizer(line);
+                    while( tokenizer.hasMoreTokens() ){
+                        String sequence = tokenizer.nextToken();
+                        if( sequence.length() == 10 ){
+                            //System.out.println(sequence);       // this is the 10 character sequence of genomes
+                            for(int i = 0; i <= sequence.length() - k; i++){
+                                String substring = sequence.substring(i, i+ k);
+                                if( substring.matches("[actgACTG]+") ){
+                                    //System.out.println(substring);
+                                    ////////
+                                    //INSERT HERE
+                                    /////// 
+                                }
+                            }
+                        }
+                    }
+                   
+                }
+            }
+        } while ( (line = br.readLine()) != null);
+
+        br.close();
+    }
 }
+/*SOURCES TO INCLUDE INTO README*/
+
+//https://stackoverflow.com/questions/19183423/why-cant-i-access-the-first-token-returned-from-javas-stringtokenizer
