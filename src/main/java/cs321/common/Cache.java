18efca52890ae5f98e32414bcc0c6cba8d0ca5e1
zachry-weber <39774817+zachry-weber@users.noreply.github.com>
2022-05-06 17:36:20 -0600

    Adds Cache.java file

---
+++ src/main/java/cs321/common/Cache.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+
+public class Cache <T>
+{
+	
+	private LinkedList<T> cache; 
+	private int size;
+	
+	public Cache(int size)
+	{
+		this.size = size;
+		this.cache = new LinkedList<T>();
+	}
+	
+	public LinkedList<T> getObject()
+	{
+		return this.cache;
+	}
+	
+	public void addObject(T e)
+	{
+		if(cache.size() == size)
+		{
+			cache.removeLast();
+		}
+		cache.addFirst(e);
+	}
+	
+	public void removeObject(T e)
+	{
+		this.cache.remove(e);
+	}
+	
+	public void clearCache()
+	{
+		this.cache.clear();
+	}
+	
+}
